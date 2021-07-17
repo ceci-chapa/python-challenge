b7663006291c5df2acea05f11947631ec310d8c5
Celenia Chapa <celeniachapa@Celenias-MBP.attlocal.net>
2021-07-17 02:30:05 -0500

    adding new loops to code

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,9 +1,9 @@
 import os
 import csv
 
-budget_csv = os.path.join("..", "Resources", "budget_data.csv")
+budget_csv = os.path.join("PyBank", "Resources", "budget_data.csv")
 
-# Lists to store data
+# Variables
 
 total_months = 0
 total = 0
@@ -16,23 +16,26 @@ greatest_decrease = []
 
 with open(budget_csv, encoding='utf-8') as csvfile:
     csv_reader = csv.reader(csvfile, delimiter=",")
-    # Read the header
+    # Skip the header
     header = next(csv_reader)
     for row in csv_reader:
-        net_change = row[1] - previous_profit
-        previous_profit = row[1]
+        net_change = previous_profit - int(row[1])
+        previous_profit = int(row[1])
 
-        #Adding the title
+        #Summing the months
         total_months += 1
-
+        
         #Adding the total months
-        total = total + row[1] 
+        total = total + int(row[1]) 
 
         #Calculating the average change
-        average_change_list.append(net_change) 
-        average_change = 
+        average_change_list.append(net_change)
 
-        
+    average_change = sum(average_change_list) / (len(average_change_list) - 1)
 
-
+    print("Financial Analysis")
+    print("----------------------------")
+    print(total_months)
+    print(total)
+    print(average_change)
 
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,9 +1,38 @@
 import os
 import csv
+from typing import TYPE_CHECKING
 
-election_csv = os.path.join("..", "Resources", "election_data.csv")
+election_csv = os.path.join("PyPoll", "Resources", "election_data.csv")
+
+# Variables
+
+total_votes = 0
+khan_percent = 0
+khan_sum = 0
+correy_sum = 0
+correy_percent = 0
+
+candidate_list = []
+
+
 
 with open(election_csv, encoding='utf-8') as csvfile:
     csv_reader = csv.reader(csvfile, delimiter=",")
+    # Skip the header
+    header = next(csv_reader)
+    for row in csv_reader:
 
-    print("Hello")
+        #Finding the totals for each candidate
+        if row[2]  == "Khan":
+            khan_sum += 1
+        
+        if row[2] == "Correy":
+            correy_sum += 1
+
+            
+        #Summing the votes
+        total_votes += 1
+
+    print(total_votes)
+    print(khan_sum)
+    print(correy_sum)
