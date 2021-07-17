a52dda60a4bf00e206720a9c01c37df53a6b2bee
Celenia Chapa <celeniachapa@Celenias-MBP.attlocal.net>
2021-07-16 23:53:43 -0500

    adding to PyBank py file

--- PyBank/main.py
+++ PyBank/main.py
@@ -4,21 +4,34 @@ import csv
 budget_csv = os.path.join("..", "Resources", "budget_data.csv")
 
 # Lists to store data
-title = []
-total_months = []
-total = []
-average_change = []
+
+total_months = 0
+total = 0
+average_change = 0
+average_change_list = []
+previous_profit = 0
 greatest_increase = []
 greatest_decrease = []
 
+
 with open(budget_csv, encoding='utf-8') as csvfile:
     csv_reader = csv.reader(csvfile, delimiter=",")
+    # Read the header
+    header = next(csv_reader)
     for row in csv_reader:
+        net_change = row[1] - previous_profit
+        previous_profit = row[1]
 
         #Adding the title
-        title.append(row[0]) = "Financial Analysis"
+        total_months += 1
 
         #Adding the total months
+        total = total + row[1] 
+
+        #Calculating the average change
+        average_change_list.append(net_change) 
+        average_change = 
+
         
 
 
