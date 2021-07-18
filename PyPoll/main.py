7901926daf52c918bbf8f6d4cec4ebb6561304ed
Celenia Chapa <celeniachapa@Celenias-MBP.attlocal.net>
2021-07-18 15:09:19 -0500

    adding most recent changes

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -3,20 +3,24 @@ import csv
 from typing import TYPE_CHECKING
 
 election_csv = os.path.join("PyPoll", "Resources", "election_data.csv")
-
+file_to_output = os.path.join("PyPoll", "Resources", "election_results.txt")
 # Variables
 
 total_votes = 0
-khan_percent = 0
-khan_sum = 0
-correy_sum = 0
-correy_percent = 0
-li_sum = 0
-li_percent = 0
-otooley_sum = 0
-otooley_percent = 0
+#khan_percent = 0
+#khan_sum = 0
+#correy_sum = 0
+#correy_percent = 0
+#li_sum = 0
+#li_percent = 0
+#otooley_sum = 0
+#otooley_percent = 0
 
-candidate_list = []
+candidate_name = []
+winner = ""
+can_votes = {}
+can_percent = 0
+
 
 
 
@@ -25,27 +29,29 @@ with open(election_csv, encoding='utf-8') as csvfile:
     # Skip the header
     header = next(csv_reader)
     for row in csv_reader:
-
-        #Finding the totals for each candidate
-        if row[2]  == "Khan":
-            khan_sum += 1
-        
-        if row[2] == "Correy":
-            correy_sum += 1
-
-        if row[2] == "Li":
-            li_sum += 1
-
-        if row[2] == "O'Tooley":
-            otooley_sum += 1
-
-        #add an if statment here for the winner 
-
         #Summing the votes
         total_votes += 1
 
-    print(total_votes)
-    print(khan_sum)
-    print(correy_sum)
-    print(li_sum)
+        #Extracting candidate name
+        can_name = row[2]
+        if can_name not in candidate_name:
+            candidate_name.append(can_name)
+            can_votes[can_name] = 0
+        
+        can_votes[can_name]= can_votes[can_name]+1
+
+with open(file_to_output, "w") as txt_file:
+    election_results = (
+       f"\n\nElection Results\n"
+      f"-------------------------\n"
+      f"Total Votes: {total_votes}\n"
+      f"-------------------------\n")
+    print(election_results, end="")
+    txt_file.write(election_results)
+    ## loop over can_votes
+        
+     ###use .get() to get votes for current candidate
+    #### calculate percentage here
+
+print(can_votes)    
 
