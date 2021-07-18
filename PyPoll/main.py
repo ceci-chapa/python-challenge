c9280e7e4344f78cb194a1254d7795d345c3c425
Celenia Chapa <celeniachapa@Celenias-MBP.attlocal.net>
2021-07-18 16:41:22 -0500

    adding latest edits

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -4,6 +4,7 @@ from typing import TYPE_CHECKING
 
 election_csv = os.path.join("PyPoll", "Resources", "election_data.csv")
 file_to_output = os.path.join("PyPoll", "Resources", "election_results.txt")
+
 # Variables
 
 total_votes = 0
@@ -40,18 +41,40 @@ with open(election_csv, encoding='utf-8') as csvfile:
         
         can_votes[can_name]= can_votes[can_name]+1
 
+             ###use .get() to get votes for current candidate
+    khan_results = can_votes.get("Khan")
+    correy_results = can_votes.get("Correy")
+    li_results = can_votes.get("Li")
+    otooley_results = can_votes.get("O'Tooley")
+
+    khan_cent = round(khan_results / total_votes, 3) * 100
+    correy_cent = round(correy_results / total_votes, 3) * 100
+    li_cent = round(li_results / total_votes, 3) * 100
+    otooley_cent = round(otooley_results / total_votes, 3) * 100
+   
+
 with open(file_to_output, "w") as txt_file:
     election_results = (
-       f"\n\nElection Results\n"
+      f"\n\nElection Results\n"
       f"-------------------------\n"
       f"Total Votes: {total_votes}\n"
-      f"-------------------------\n")
+      f"-------------------------\n"
+      f"Khan: {khan_cent} ({khan_results})\n"
+      f"Khan: {correy_cent} ({correy_results})\n"
+      f"Li: {li_cent} ({li_results})\n"
+      f"O'Tooley: {otooley_cent} ({otooley_results})\n")
+
     print(election_results, end="")
     txt_file.write(election_results)
-    ## loop over can_votes
-        
-     ###use .get() to get votes for current candidate
-    #### calculate percentage here
 
-print(can_votes)    
+    ## loop over can_votes
+    
+     ###use .get() to get votes for current candidate
+    #khan_results = can_votes.get("Khan")
+
+    #khan_cent = round(khan_results / total_votes, 3) * 100
+    #print(f'Khan: {khan_cent} {khan_results}')
+
+
+    #### calculate percentage here   
 
